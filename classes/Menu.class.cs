f250078c1095011946e03c84bd05380b55e372c7
Nathanael Shermett <nathanael@shermett.me>
2020-02-12 11:06:59 -0500

    Added classes Action, Menu, MenuItem, and Universe. Corrected enum file names.

--- classes/Menu.class.cs
+++ classes/Menu.class.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SpaceGame
 {
 	public class Menu
 	{
-		
+		public List<MenuItem> MenuItems
+		{
+			get; set;
+		}
+
+		public Menu()
+		{
+			this.MenuItems = new List<MenuItem>();
+		}
+
+		public Menu(List<MenuItem> menuItems)
+		{
+			this.MenuItems = menuItems;
+		}
+
+		public void Add(MenuItem item)
+		{
+			this.MenuItems.Add(item);
+		}
 	}
 }
---
+++ classes/MenuItem.class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaceGame
+{
+	public class MenuItem
+	{
+		public string Label
+		{
+			get; set;
+		}
+
+		public Action Action
+		{
+			get; set;
+		}
+
+		public MenuItem(string label, Action action)
+		{
+			this.Label = label;
+			this.Action = action;
+		}
+
+		public void Execute()
+		{
+			this.Action.Execute();
+		}
+	}
+}
