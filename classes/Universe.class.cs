27fe20ed25293704e0a14c06053f72f005253842
Justin N Davis <Justin.N.Davis@outlook.com>
2020-02-12 11:10:51 -0500

    added Universe class

---
+++ classes/Universe.class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceGame.classes
+{
+    class Universe
+    {
+        public Objective[] Objectives;
+        public Character Character;
+        public CelestialBody CelestialBody;
+
+        public Universe(Objective[] objectives, Character character, CelestialBody celestialBody)
+        {
+            this.Objectives = objectives;
+            this.Character = character;
+            this.CelestialBody = celestialBody;
+        }
+    }
+}
