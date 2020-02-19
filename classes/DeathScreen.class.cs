dff85b78e338dba9c2cf3aef2af34e41b1d18693
Justin N Davis <Justin.N.Davis@outlook.com>
2020-02-19 08:46:01 -0500

    added opening sequence and new classes

---
+++ classes/DeathScreen.class.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceGame
+{
+    class DeathScreen
+    {
+        string deathScreen = @"                                                                   
+                           .AMMMMMMMMMMA.          
+      			.AV. :::.:.:.::MA.        
+      	  	       A' :..        : .:`A       
+   		      A'..              . `A.                   YY   YY      A
+   		     A' :.    :::::::::  : :`A                   YY YY      AAA
+   		     M  .    :::.:.:.:::  . .M                    YYY      A   A
+  		     M  :   ::.:.....::.:   .M                     Y      AAAAAAA
+  		     V : :.::.:........:.:  :V                     Y      A     A
+ 		    A  A:    ..:...:...:.   A A                    Y      A     A
+ 		   .V  MA:.....:M.::.::. .:AM.M                    Y      A     A
+		  A'  .VMMMMMMMMM:.:AMMMMMMMV: A  
+		 :M .  .`VMMMMMMV.:A `VMMMMV .:M: 
+		  V.:.  ..`VMMMV.:AM..`VMV' .: V  
+		   V.  .:. .....:AMMA. . .:. .V            DDDDDDD    EEEEEEE      A      DDDDDDD
+ 		    VMM...: ...:.MMMM.: .: MMV             D     DD   E           AAA     D     DD
+		       `VM: . ..M.:M..:::M'                D      D   E          A   A    D      D
+   			 `M::. .:.... .::M                 D      D   EEEEEEE   AAAAAAA   D      D
+		          M:.  :. .... ..M                 D     DD   E         A     A   D      D
+		          V:  M:. M. :M .V                 D    DD    E         A     A   D     DD
+		          `V.:M.. M. :M.V'                 DDDDDD     EEEEEEE   A     A   DDDDDDD
+                                                                   
+                                                                   
+                                                                   ";
+    }
+}
